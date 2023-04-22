2f94d5cffbe86e36e5233ca28c20204bfc853222
Md Shohel Rana <113632658+Md-Shohel-Rana@users.noreply.github.com>
2023-04-23 00:50:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,3 @@ const server=http.createServer((req,res)=>{
 server.listen(PORT,()=>{
     console.log('server running successfully');
 });
-server.listen(PORT,()=>{
-    console.log('server running successfully');
-});
